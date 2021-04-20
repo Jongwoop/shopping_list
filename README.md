c0759ea50cb3af00f7a33f517daab75e26af301d
Jongwoop <58589069+Jongwoop@users.noreply.github.com>
2021-04-21 00:49:44 +0300

    Last touch + Updated Readme file

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # shopping_list
-Making brief shopping list with HTML, CSS and Javascript
+
+Making brief shopping list
+
+## Stack used
+
+HTML, CSS and Javascript
+
+## Main Function
+
+**1. Bring the text from user input**</br>
+**2. Making new item(text + remove btn)**</br>
+**3. Add new item in the item container**</br>
+**4. Renew the input box**
+
+## Screenshot
+
+![Image of Screenshot]
+(./screenshot.png)
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const items = document.querySelector('.items')
-const addBtn = document.querySelector('.addBtn')
-const input = document.querySelector('.input')
+const addBtn = document.querySelector('.footer__addBtn')
+const input = document.querySelector('.footer__input')
 
 
 function add() {
@@ -9,36 +9,42 @@ function add() {
     input.focus()
     return;
   }
-  createItem(text);
-  console.log(text);
+  item = createItem(text);
+  items.appendChild(item);
+  item.scrollIntoView({block: 'center'});
 
   input.value = '';
   input.focus();
 }
 
 function createItem(text) {
-  const newItem = document.createElement("div");
-  newItem.setAttribute("class", "item");
+  const itemRow = document.createElement("li");
+  itemRow.setAttribute("class", "item__row");
+  
+  const item = document.createElement("div");
+  item.setAttribute("class", "item");
 
   const name = document.createElement('span');
   name.setAttribute("class", "item__name" );
   name.innerText = text;
 
   const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute("class", "deleteBtn")
+  deleteBtn.setAttribute("class", "item__delete")
   deleteBtn.innerHTML = `<i class="far fa-trash-alt"></i>`
   deleteBtn.addEventListener('click', ()=>{
-    items.removeChild(newItem);
-    items.removeChild(divider);
+    items.removeChild(itemRow);
   })
 
   const divider = document.createElement("div");
-  divider.setAttribute("class", "divider");
+  divider.setAttribute("class", "item__divider");
 
-  items.appendChild(newItem);
-  newItem.appendChild(name);
-  newItem.appendChild(deleteBtn);
-  items.appendChild(divider);  
+  itemRow.appendChild(item);
+  itemRow.appendChild(divider);  
+  
+  item.appendChild(name);
+  item.appendChild(deleteBtn);
+
+  return itemRow;
 }
 
 addBtn.addEventListener('click', () =>{
